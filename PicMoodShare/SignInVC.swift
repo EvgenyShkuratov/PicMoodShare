f8285024f27f4d0ec5611d23a3bf2226f0832396
EvgenyShkuratov <evgenyshkuratov@Sujins-MacBook-Pro.local>
2016-12-01 13:32:47 -0800

    Facebook auth completed

--- PicMoodShare/SignInVC.swift
+++ PicMoodShare/SignInVC.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("EVGY: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("EVGY: User cancelled authentication")
+            } else {
+                print("EVGY: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("EVGY: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("EVGY: Successfully authenticated with Firebase")
+            }
+        })
+    }
+    
+    
+    
 }
 
