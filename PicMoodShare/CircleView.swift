89deef7013cc5d4c5543b6cf42724f04d02fe949
EvgenyShkuratov <evgenyshkuratov@Sujins-MacBook-Pro.local>
2016-12-02 13:06:41 -0800

    Feed UI part 1 complete

--- PicMoodShare/CircleView.swift
+++ PicMoodShare/CircleView.swift
@@ -1,14 +1,14 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  PicMoodShare
 //
-//  Created by Evgeny Shkuratov on 11/29/16.
+//  Created by Evgeny Shkuratov on 12/1/16.
 //  Copyright © 2016 Evgeny Shkuratov. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,7 +17,11 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
